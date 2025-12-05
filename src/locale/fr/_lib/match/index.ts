2137c57b682c4eaaca206aa3791a1cebcf5fb2f7
Sanjana <sanju250506@gmail.com>
2025-12-05 22:37:34 +0530

    fixed the date issue

--- src/locale/fr/_lib/match/index.ts
+++ src/locale/fr/_lib/match/index.ts
@@ -27,7 +27,7 @@ const parseQuarterPatterns = {
 const matchMonthPatterns = {
   narrow: /^[jfmasond]/i,
   abbreviated:
-    /^(janv?|févr?|mars|avr|mai|juin|juill?|août|sept|oct|nov|déc)\.?/i,
+    /^(janv?|f[eé]vr?|mars?|avr|mai|juin|juill?|ao[uû]t|sept?|oct|nov|d[eé]c)\.?/i,
   wide: /^(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)/i,
 };
 const parseMonthPatterns = {
